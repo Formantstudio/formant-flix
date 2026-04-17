57fda09bd09374e370e4971e2ac91f2650f789b6
TheBudderMan <skepsisdrumandbass@gmail.com>
2026-04-16 22:17:04 -0500

    Move Firebase config to env vars

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,18 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
+if (import.meta.env.DEV) {
+  // @ts-ignore
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsvg-Bz3o2qf2r_zrXuSLfMQJ8u67tG2k",
-  authDomain: "formant-flix.firebaseapp.com",
-  projectId: "formant-flix",
-  storageBucket: "formant-flix.firebasestorage.app",
-  messagingSenderId: "232649287099",
-  appId: "1:232649287099:web:93f23bc0cdbb34241d109e",
-  measurementId: "G-DL0GX10BZL",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
 };
 
 export const app = initializeApp(firebaseConfig);
+
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LehhbssAAAAAKAnDntzpVxh0m63vXbeZC7SIqp8"),
+  isTokenAutoRefreshEnabled: true,
+});
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
